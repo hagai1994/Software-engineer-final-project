0fc3713721e0d394a164c0807363db14efb73f6e
HagaiCo <50545614+hagai1994@users.noreply.github.com>
2020-04-03 16:02:06 +0300

    testing

--- src/helloworld.java
+++ src/helloworld.java
@@ -1,4 +1,9 @@
 
 public class helloworld {
-
+	public static void main(String args[])  
+    {  
+        System.out.print("first statement. ");  
+        System.out.print("second statement. ");  
+        System.out.print("third statement");  
+    } 
 }
